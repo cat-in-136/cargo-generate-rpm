bc322e6bb1bd7d04e1df12123abd9a580e5f0b8d
cat_in_136 <cat.in.136+github@gmail.com>
2020-09-21 17:42:22 +0900

    fix error handling of executable

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
 use crate::config::Config;
+use crate::error::Error;
 use std::fs::File;
 
 mod config;
 mod error;
 
-fn main() {
-    let config = Config::new("Cargo.toml").unwrap();
+fn process() -> Result<(), Error> {
+    let config = Config::new("Cargo.toml")?;
 
-    let rpm_pkg = config.create_rpm_builder("x86_64").unwrap()
-        .build().unwrap();
+    let rpm_pkg = config.create_rpm_builder("x86_64")?.build()?;
     let mut f = File::create("target/package.rpm").unwrap(); // TODO get package name
-    rpm_pkg.write(&mut f).unwrap();
+    rpm_pkg.write(&mut f)?;
+
+    Ok(())
+}
+
+fn main() {
+    if let Some(_) = std::env::var_os("RUST_BACKTRACE") {
+        process().unwrap();
+    } else {
+        let result = process();
+        if let Err(err) = result {
+            eprintln!("cargo-binary-rpm: {}", err);
+            std::process::exit(1);
+        }
+    }
 }
