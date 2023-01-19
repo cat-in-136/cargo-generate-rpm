a76aba4c32fde9a7bcdb2e1b9e0a93718ac11f5a
Bernhard Schuster <bernhard@ahoi.io>
2023-01-19 18:16:31 +0100

    add /bin/sh as rpm-rs did before

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -194,6 +194,8 @@ impl Config {
             builder = builder.post_uninstall_script(post_uninstall_script);
         }
 
+        builder = builder.requires(Dependency::any("/bin/sh".to_string()));
+
         if let Some(requires) = metadata.get_table("requires")? {
             for dependency in Self::table_to_dependencies(requires)? {
                 builder = builder.requires(dependency);
